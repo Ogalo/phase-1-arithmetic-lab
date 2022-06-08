97a64fb628dd24560da70793f1ea053ddaab61ed
Otieno Harrison Omondi <ogaloharrison@gmail.com>
2022-06-08 18:41:05 +0300

    commit tree 73fa27673fd63a339608bcb568a32b62fc025452

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
 
 // Write your code here
+let num1 = 31;
+let num2 = 2;
+let num3 = 54;
+let num4 = 5;
+
+const multiply = num1*num2;
+const random = Math.floor(Math.random() *1000 + 1);
+const mod = num3 % num4;
+
+const max = Math.max(1, 5, 20, 17);
